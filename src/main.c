fb31e6aec22af719d3eeaa7a38b952a84f7b452a
Jayashree Pradhan <mrbob6645@gmail.com>
2019-08-14 13:44:47 -0400

    added stopwatch feature

--- src/main.c
+++ src/main.c
@@ -23,19 +23,27 @@
 #define FONT_HEIGHT 8
 /* Put your function prototypes here */
 void dispSprite_Centered(gfx_sprite_t *sprite);
-void dispText(char *text);
+void print_Time(float elasped);
 /* Put all your globals here */
 
 void main(void) {
-       	char *time = "00.00";
 	gfx_Begin();
 	gfx_SetPalette(logo_gfx_pal, sizeof_logo_gfx_pal, 0);
 	gfx_SetTextFGColor(255);
 	gfx_FillScreen(0);
-	gfx_SetTextTransparentColor(0);
+	gfx_SetTextTransparentColor(1);
 	gfx_SetTextBGColor(0);
 	dispSprite_Centered(rubikcube);
-	dispText(time);
+	print_Time(0.0f);
+	timer_Control = TIMER1_DISABLE;
+	timer_1_Counter = 0;
+	while (!os_GetCSC());
+	timer_Control = TIMER1_ENABLE | TIMER1_32K | TIMER1_UP;	
+	do {
+		float elapsed = (float)atomic_load_increasing_32(&timer_1_Counter) / 32768;
+		print_Time(elapsed);
+
+	} while (!os_GetCSC());
 	while (!os_GetCSC());
 	gfx_End();
 
@@ -46,6 +54,11 @@ void dispSprite_Centered(gfx_sprite_t *sprite) {
 	gfx_Sprite(sprite, (LCD_WIDTH - 60) / 2, (LCD_HEIGHT - 60) / 2);
 }
 
-void dispText(char *text) {
-	gfx_PrintStringXY(text, (LCD_WIDTH - gfx_GetStringWidth(text)) / 2, (LCD_HEIGHT - FONT_HEIGHT) / 2+60);
+void print_Time(float elapsed) {
+	real_t elapsed_real;
+	char str[10];
+	elapsed_real = os_FloatToReal(elapsed <= 0.001f ? 0.0f : elapsed);
+	os_RealToStr(str, &elapsed_real, 8, 1, 2);
+	gfx_PrintStringXY(str, (LCD_WIDTH - gfx_GetStringWidth(str)) / 2, (LCD_HEIGHT - FONT_HEIGHT) / 2+60);
+
 }
